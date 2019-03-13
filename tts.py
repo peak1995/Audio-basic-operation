80a802064b1c5ffb05f4ce2ee3dadf9ddd03b76c
peak <chengaofeng1109@163.com>
2019-03-13 12:04:51 +0800

    Add files via upload

--- tts.py
+++ tts.py
@@ -1,50 +1,116 @@
-# coding:utf-8
+import sys
+import json
 
-import pyttsx3
+IS_PY3 = sys.version_info.major == 3
+if IS_PY3:
+    from urllib.request import urlopen
+    from urllib.request import Request
+    from urllib.error import URLError
+    from urllib.parse import urlencode
+    from urllib.parse import quote_plus
+else:
+    import urllib2
+    from urllib import quote_plus
+    from urllib2 import urlopen
+    from urllib2 import Request
+    from urllib2 import URLError
+    from urllib import urlencode
 
-# 初始化
-engine = pyttsx3.init()
+API_KEY = '8dzl26wEUgFVrxDbWuwe7D90'
+SECRET_KEY = '58fmkf3UThBGdQuL0ujwvqMFUXC5AdXM'
 
-voices = engine.getProperty('voices')
-# 语速控制
-rate = engine.getProperty('rate')
-print(rate)
-engine.setProperty('rate', rate - 20)
+TEXT = "欢迎使用百度语音合成。";
 
-# 音量控制
-volume = engine.getProperty('volume')
-print(volume)
-engine.setProperty('volume', volume - 0.25)
+# 发音人选择, 0为普通女声，1为普通男生，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
+PER = 4
+# 语速，取值0-15，默认为5中语速
+SPD = 5
+# 音调，取值0-15，默认为5中语调
+PIT = 5
+# 音量，取值0-9，默认为5中音量
+VOL = 5
+# 下载的文件格式, 3：mp3(default) 4： pcm-16k 5： pcm-8k 6. wav
+AUE = 3
 
-engine.say('hello world')
-engine.say('你好，世界')
-# 朗读一次
-engine.runAndWait()
+FORMATS = {3: "mp3", 4: "pcm", 5: "pcm", 6: "wav"}
+FORMAT = FORMATS[AUE]
 
-engine.say('语音合成开始')
-engine.say('我会说中文了，开森，开森')
-engine.runAndWait()
+CUID = "123456PYTHON"
 
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
+TTS_URL = 'http://tsn.baidu.com/text2audio'
 
-# 更换语种--zh_HK
-engine.setProperty('voice', "com.apple.speech.synthesis.voice.sin-ji")
-engine.setProperty('voice', "VoiceGenderMale")
-engine.say('从方法声明上来看，第一个参数指定的是语音驱动的名称，这个在底层适合操作系统密切相关的。')
 
-# 更换语种--zh_CN
-engine.setProperty('voice', "com.apple.speech.synthesis.voice.ting-ting.premium")
-engine.say('从方法声明上来看，第一个参数指定的是语音驱动的名称，这个在底层适合操作系统密切相关的。')
+class DemoError(Exception):
+    pass
 
-# 更换语种--zh_TW
-engine.setProperty('voice', "com.apple.speech.synthesis.voice.mei-jia")
-engine.say('从方法声明上来看，第一个参数指定的是语音驱动的名称，这个在底层适合操作系统密切相关的。')
-engine.runAndWait()
 
-for voice in voices:
-    print(voice)
-    engine.setProperty("voice", voice.id)
+"""  TOKEN start """
 
-# engine.endLoop()
+TOKEN_URL = 'http://openapi.baidu.com/oauth/2.0/token'
+SCOPE = 'audio_tts_post'  # 有此scope表示有tts能力，没有请在网页里勾选
 
+
+def fetch_token():
+    print("fetch token begin")
+    params = {'grant_type': 'client_credentials',
+              'client_id': API_KEY,
+              'client_secret': SECRET_KEY}
+    post_data = urlencode(params)
+    if (IS_PY3):
+        post_data = post_data.encode('utf-8')
+    req = Request(TOKEN_URL, post_data)
+    try:
+        f = urlopen(req, timeout=5)
+        result_str = f.read()
+    except URLError as err:
+        print('token http response http code : ' + str(err.code))
+        result_str = err.read()
+    if (IS_PY3):
+        result_str = result_str.decode()
+
+    print(result_str)
+    result = json.loads(result_str)
+    print(result)
+    if ('access_token' in result.keys() and 'scope' in result.keys()):
+        if not SCOPE in result['scope'].split(' '):
+            raise DemoError('scope is not correct')
+        print('SUCCESS WITH TOKEN: %s ; EXPIRES IN SECONDS: %s' % (result['access_token'], result['expires_in']))
+        return result['access_token']
+    else:
+        raise DemoError('MAYBE API_KEY or SECRET_KEY not correct: access_token or scope not found in token response')
+
+
+"""  TOKEN end """
+
+if __name__ == '__main__':
+    token = fetch_token()
+    tex = quote_plus(TEXT)  # 此处TEXT需要两次urlencode
+    print(tex)
+    params = {'tok': token, 'tex': tex, 'per': PER, 'spd': SPD, 'pit': PIT, 'vol': VOL, 'aue': AUE, 'cuid': CUID,
+              'lan': 'zh', 'ctp': 1}  # lan ctp 固定参数
+
+    data = urlencode(params)
+    print('test on Web Browser' + TTS_URL + '?' + data)
+
+    req = Request(TTS_URL, data.encode('utf-8'))
+
+    has_error = False
+    try:
+        f = urlopen(req)
+        result_str = f.read()
+
+        has_error = ('Content-Type' not in f.headers.keys() or f.headers['Content-Type'].find('audio/') < 0)
+    except  URLError as err:
+        print('asr http response http code : ' + str(err.code))
+        result_str = err.read()
+        has_error = True
+
+    save_file = "error.txt" if has_error else 'result.' + FORMAT
+    with open(save_file, 'wb') as of:
+        of.write(result_str)
+
+    if has_error:
+        if (IS_PY3):
+            result_str = str(result_str, 'utf-8')
+        print("tts api  error:" + result_str)
+    print("result saved as :" + save_file)
